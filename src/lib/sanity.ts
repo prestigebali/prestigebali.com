2ed36accd7128d533e6f917bb8138192cb42487c
Prestige Bali <prestigebali.webdev@gmail.com>
2025-10-17 14:29:56 +0000

    Try fixing this error: `Runtime Error: Error: Request error while attemp

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -1,9 +1,6 @@
 import {createClient} from '@sanity/client'
 import imageUrlBuilder from '@sanity/image-url'
-
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!
-export const apiVersion = '2024-05-01'
+import {projectId, dataset, apiVersion} from '../../sanity.config'
 
 export const client = createClient({
   projectId,
