396aec3b329706c2a244a6090ce6ce17f21922bb
Prestige Bali <prestigebali.webdev@gmail.com>
2025-10-18 00:17:25 +0000

    Bisakah kamu rubah slug SanityCMS dari /studio ke /admin ?

--- sanity.config.ts
+++ sanity.config.ts
@@ -6,7 +6,7 @@ import {deskStructure} from './sanity/deskStructure'
 import {projectId, dataset, apiVersion} from './src/lib/sanity-client-config'
 
 export default defineConfig({
-  basePath: '/studio',
+  basePath: '/admin',
   name: 'prestige_bali_studio',
   title: 'Prestige Bali Studio',
   projectId,
