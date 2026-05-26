aa3080aa64655513660794b1b66af5a764871b37
prestigebali <prestigebali.webdev@gmail.com>
2026-05-26 14:28:35 +0800

    Update sanity.ts

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -1,11 +1,11 @@
 import {createClient} from 'next-sanity'
 import imageUrlBuilder from '@sanity/image-url'
-import {projectId, dataset, apiVersion} from './sanity-client-config'
+import {projectId, dataset} from './sanity-client-config'
 
 export const client = createClient({
   projectId: projectId || 'placeholder',
   dataset: dataset || 'production',
-  apiVersion: apiVersion || '2024-05-01',
+  apiVersion: '2024-05-01',
   useCdn: process.env.NODE_ENV === 'production',
   next: {
     revalidate: process.env.NODE_ENV === 'production' ? 3600 : false,
